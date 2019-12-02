f6e913f6e8f2c65cab2d8d860a412cb820b92a2b
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2019-12-02 15:37:32 +0900

    optimize. failed to save saved model

--- tfjs/save_graph.py
+++ tfjs/save_graph.py
@@ -3,11 +3,11 @@ import argparse
 import json
 import numpy as np
 import tensorflow as tf
-sys.path.append('../python')
+sys.path.append("../python")
 import common
 from model import Model
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pos_len = 19
     description = """
     Play go with a trained neural net!
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description=description)
     common.add_model_load_args(parser)
-    parser.add_argument('-name-scope', help='Name scope for model variables', required=False)
+    parser.add_argument("-name-scope", help="Name scope for model variables", required=False)
     args = vars(parser.parse_args())
 
     (model_variables_prefix, model_config_json) = common.load_model_paths(args)
@@ -28,13 +28,13 @@ if __name__ == '__main__':
         with tf.name_scope(name_scope):
             model = Model(model_config,pos_len,{
                   "is_training": tf.constant(False,dtype=tf.bool),
-                  "symmetries": tf.constant(False, shape=[3], dtype=tf.bool),
+                  #"symmetries": tf.constant(False, shape=[3], dtype=tf.bool),
                   "include_history": tf.constant(1.0, shape=[1,5], dtype=tf.float32)
             })
     else:
         model = Model(model_config,pos_len,{
                   "is_training": tf.constant(False,dtype=tf.bool),
-                  "symmetries": tf.constant(False, shape=[3], dtype=tf.bool),
+                  #"symmetries": tf.constant(False, shape=[3], dtype=tf.bool),
                   "include_history": tf.constant(1.0, shape=[1,5], dtype=tf.float32)
         })
 
@@ -45,19 +45,4 @@ if __name__ == '__main__':
 
     with tf.Session() as session:
         saver.restore(session, model_variables_prefix)
-        tf.compat.v1.saved_model.simple_save(
-            session,
-            './saved_model',
-            inputs={
-                "swa_model/bin_inputs": model.bin_inputs,
-                "swa_model/global_inputs": model.global_inputs,
-            },
-            outputs={
-                'swa_model/policy_output': model.policy_output,
-                'swa_model/value_output': model.value_output,
-                'swa_model/miscvalues_output': model.miscvalues_output,
-                'swa_model/scorebelief_output': model.scorebelief_output,
-                'swa_model/bonusbelief_output': model.bonusbelief_output,
-                'swa_model/ownership_output': model.ownership_output,
-            }
-        )
+        tf.train.write_graph(session.graph,"./tmp","graph.pbtxt", as_text=True)
